3864d8e16760fcb577512b32e45f5b343bc91d8c
utsav <sav.acharya22@gmail.com>
2024-04-20 07:51:01 +1000

    add file helper js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body>
         {children}
-
+      <footer>
+        Page rendered on {new Date().toLocaleString()}
+      </footer>
       </body>
     </html>
   );
