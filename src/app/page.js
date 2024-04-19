d56f36bace0df923460f06ef1d1ca5639dd39535
utsav <sav.acharya22@gmail.com>
2024-04-20 08:04:05 +1000

    add hits implementation

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import { readFile, writeFile } from "@/helpers/file-helpers";
+import React from "react";
+
+const DATABASE_PATH = "/src/database.json";
 
 function Home() {
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
+
+  hits += 1;
+
+  writeFile(DATABASE_PATH, JSON.stringify({ hits }));
   return (
     <main>
       <h1>Hello Next!</h1>
+      <p>You are visitor number: {hits}</p>
     </main>
   );
 }
